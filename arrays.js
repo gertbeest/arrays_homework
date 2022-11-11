e2bb28519900b2ac81a79b3fcfcaa0ce9fd69e62
Alex Alexander <a.alexander62442@gmail.com>
2022-11-10 19:55:47 -0700

    Add 3 array methods

--- arrays.js
+++ arrays.js
@@ -5,3 +5,16 @@ var hasRsvped = [true, false, false, true]
 console.log(fridge)
 console.log(pins)
 console.log (hasRsvped)
+
+//pop will remove the last element from the fridge array (gatorade)
+fridge.pop()
+console.log(fridge)
+
+//push will add these three new pins to the end of the pins array
+pins.push(1987, 1337, 6543)
+console.log(pins)
+
+//shift will remove the first element from the hasRsvped array - after shift removes the first element, 
+//it shifts the remaining 3 to the left in position
+hasRsvped.shift()
+console.log(hasRsvped)
