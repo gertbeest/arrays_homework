e07d8be532ee6fd0591099eb930d34d308316883
Alex Alexander <a.alexander62442@gmail.com>
2022-11-10 19:39:07 -0700

    Add 3 variables

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var fridge = ["milk", "yogurt", "pepperonis", "gatorade"]
+var pins = [1234, 4321, 5678, 8765]
+var hasRsvped = [true, false, false, true]
+
+console.log(fridge)
+console.log(pins)
+console.log (hasRsvped)
